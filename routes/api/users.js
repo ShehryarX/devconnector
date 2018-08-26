0c5649b4fe0f4bed2923f06974e741a036fd97e7
Shehryar Assad <xshehryar@gmail.com>
2018-08-26 12:48:53 -0400

    Add register and login validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,10 @@ const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
 
+// Load input validation
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
+
 // load User model
 const User = require("../../models/User").User;
 
@@ -18,8 +22,16 @@ router.get("/test", (req, res) => res.json({ message: "Users works" }));
 // @desc    Register user
 // @access  Public
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // check validation
+  if (!isValid) return res.status(400).json(errors);
+
   User.findOne({ email: req.body.email }).then(user => {
-    if (user) return res.status(400).json({ email: "Email already exists" });
+    if (user) {
+      errors.email = "Email already exists";
+      return res.status(400).json(errors);
+    }
 
     const { name, email, password } = req.body;
     const avatar = gravatar.url(email, {
@@ -52,15 +64,27 @@ router.post("/register", (req, res) => {
 // @desc    Login user and return JWT token
 // @access  Public
 router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // check validation
+  if (!isValid) return res.status(400).json(errors);
+
   const { email, password } = req.body;
 
   // find user by email
   User.findOne({ email }).then(user => {
-    if (!user) return res.status(404).json({ email: "User not found" }); // user not found
+    if (!user) {
+      // user not found
+      errors.email = "User not found";
+      return res.status(404).json(errors);
+    }
 
     bcrypt.compare(password, user.password).then(isMatched => {
-      if (isMatched)
-        return res.status(400).json({ password: "Password incorrect" });
+      console.log(isMatched);
+      if (!isMatched) {
+        errors.password = "Password incorrect";
+        return res.status(400).json(errors);
+      }
 
       // user matched
       const { id, name, avatar } = user;
