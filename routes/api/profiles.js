5abc9d620ad26a2b094c84a382d646e701b91811
Shehryar Assad <xshehryar@gmail.com>
2018-08-27 22:13:55 -0400

    Add fetch all profiles and fetch by handle and user ID route

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -40,6 +40,69 @@ router.get(
   }
 );
 
+// @route   GET api/profiles/all
+// @desc    Get all profiles
+// @access  Public
+router.get("/all", (req, res) => {
+  const errors = {};
+
+  Profile.find()
+    .populate("user", ["name", "avatar"])
+    .then(profiles => {
+      if (!profiles) {
+        errors.noprofiles = "There are no profiles";
+        return res.status(404).json(errors);
+      } else {
+        res.json(profiles);
+      }
+    })
+    .catch(err =>
+      res.status(404).json({ noprofiles: "There are no profiles" })
+    );
+});
+
+// @route   GET api/profiles/handle/:handle
+// @desc    Get profile by handle
+// @access  Public
+router.get("/handle/:handle", (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ handle: req.params.handle })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user";
+        res.status(404).json(errors);
+      } else {
+        res.json(profile);
+      }
+    })
+    .catch(err =>
+      res.status(404).json({ noprofile: "There is no profile for this user" })
+    );
+});
+
+// @route   GET api/profiles/user/:user_id
+// @desc    Get profile by user ID
+// @access  Public
+router.get("/user/:user_id", (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ user: req.params.user_id })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user";
+        res.status(404).json(errors);
+      } else {
+        res.json(profile);
+      }
+    })
+    .catch(err =>
+      res.status(404).json({ noprofile: "There is no profile for this user" })
+    );
+});
+
 // @route   POST api/profiles/
 // @desc    Create or edit user profile
 // @access  Private
@@ -53,7 +116,6 @@ router.post(
     if (!isValid) return res.status(400).json(errors);
 
     const {
-      id,
       handle,
       company,
       website,
@@ -91,13 +153,13 @@ router.post(
     if (facebook) profileFields.social.facebook = facebook;
     if (instagram) profileFields.social.instagram = instagram;
 
-    // profileFields.user = req.user.id;
+    // TODO: Accounts exists and handle wanted exists too!
 
-    Profile.findOne({ user: id }).then(profile => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
       if (profile) {
         // update profile
         Profile.findOneAndUpdate(
-          { user: id },
+          { user: req.user.id },
           { $set: profileFields },
           { new: true }
         ).then(profile => res.json(profile));
